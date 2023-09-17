cfcee7f450fa177f9d3767004ad072b69212f59d
pmason <philipmasondev@gmail.com>
2023-09-16 23:45:07 -0400

    added backend file, frontend file, .gitignore

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    console.log(`test`)
+});
+
