646d6da8d1c1e924f941c7a0684ae7a82375113b
pmason <philipmasondev@gmail.com>
2023-09-17 10:54:00 -0400

    added connection to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
+const dotenv = require("dotenv").config()
 const express = require("express")
+const connectDB = require("./config/connectDB")
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-    res.send('<body style="background-color: gray;"><h1>hello world</h1></body>')
-})
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
-    
     console.log(`Server running on port ${PORT}`)
-    
 });
 
+// Do not start server if connection to DB fails. 
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.get('/', (req, res) => {
+            res.send('<body style="background-color: gray;"><h1>hello world</h1></body>')
+        })
+        
+    } catch (error) {
+        console.log(`Error with connecting to MongoDB: ${error}`)
+    }
+};
+
+// Start server
+startServer();
