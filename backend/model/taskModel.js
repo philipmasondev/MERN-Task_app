fc8e565836d1c9615130f491a3c6c6f9b21200f1
pmason <philipmasondev@gmail.com>
2023-09-17 11:59:04 -0400

    added db model

---
+++ backend/model/taskModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const taskSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "please add a task"]
+        },
+        completed: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timeStamps: true,
+
+    }
+)
+const Task = mongoose.model("Task", taskSchema)
+
+module.exports = Task
